ace1f30611ad9a3a82cecb98a49ff43ead4dfdf5
Benedikt Böhm <bb@xnull.de>
2009-08-27 13:33:09 +0200

    resolve FQDN via DNS. should fix #2085 and #2573

--- lib/facter/fqdn.rb
+++ lib/facter/fqdn.rb
@@ -1,11 +1,7 @@
+require 'socket'
+
 Facter.add(:fqdn) do
     setcode do
-        host = Facter.value(:hostname)
-        domain = Facter.value(:domain)
-        if host and domain
-            [host, domain].join(".")
-        else
-            nil
-        end
+        Socket.gethostbyname(Socket.gethostname).first
     end
 end
